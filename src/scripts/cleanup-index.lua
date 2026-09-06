8bb0b583d72bcf6d2068f808ee0de7f2bded778a
Anwarkamal143 <anwarkamal1434@gmail.com>
2026-09-07 00:13:19 +0500

    removed scripts

--- src/scripts/cleanup-index.lua
+++
@@ -1,4 +0,0 @@
--- KEYS[1] user index
--- ARGV[1..N] stale token hashes (user-index ZSET members, not JTIs)
-for i=1,#ARGV do redis.call('ZREM', KEYS[1], ARGV[i]) end
-return #ARGV
--- src/scripts/delete.lua
+++
@@ -1,2 +0,0 @@
--- KEYS[1] session key
-return redis.call('DEL', KEYS[1])
